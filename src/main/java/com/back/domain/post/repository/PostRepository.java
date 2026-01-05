00584ce44bfc3f8f20b5a015a03cc911b6fe7850
rkdls8268 <rkdls8268@gmail.com>
2026-01-05 15:33:45 +0900

    0011 - Post 생성 기능 구현 (create)

--- src/main/java/com/back/domain/post/repository/PostRepository.java
+++ src/main/java/com/back/domain/post/repository/PostRepository.java
@@ -1,7 +1,6 @@
 package com.back.domain.post.repository;
 
 import com.back.domain.post.document.Post;
-import com.back.domain.post.document.Post;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
 public interface PostRepository extends ElasticsearchRepository<Post,String> {
